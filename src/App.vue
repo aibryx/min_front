fbd8ecb1827d4c87835e26d47f75a768b6620e47
alimdzhnibekov1@gmail.com <Zxcvbnm0900>
2023-03-11 19:28:44 +0300

    init

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,35 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div id="app">
+    <button class="btn" @click="login">Войти</button>
+    <div class="response">Response  {{ response }}</div>
+  </div>
 </template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      response: ""
+    }
+  },
+  methods: {
+    async login() {
+      const url = "https://dev.milkhunters.ru/api/v1/docs/"
+
+      const response = await axios.post(url + "signin", {
+        "username": "supertest",
+        "password": "qwerty1234@"
+      })
+
+      this.response = response.data
+
+      console.log(response.data)
+    }
+  }
+}
+</script>
 
 <style>
 #app {
@@ -26,5 +51,26 @@ nav a {
 
 nav a.router-link-exact-active {
   color: #42b983;
+}
+
+.btn {
+  background: teal;
+  border: 2px solid teal;
+  border-radius: 5px;
+  color: white;
+  width: 100px;
+  height: 30px;
+}
+
+.response {
+  background: teal;
+  border: 2px solid teal;
+  border-radius: 5px;
+  color: white;
+  width: 100px;
+  height: 30px;
+  padding: 10px;
+  display: flex;
+  align-items: center;
 }
 </style>
